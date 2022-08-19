1aad28439ea4e5b599dbcf6e9b8fe4cfb57994bf
lvnguyen.ivs-1217 <lvnguyen.1217@indivisys.jp>
2022-08-19 13:53:18 +0700

    add loading file and scss

---
+++ src/components/loadding.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import '../../style'
+
+export default class loadding extends Component {
+  render() {
+    return ( 
+        <div className='loadding'></div>
+    )
+  }
+}
